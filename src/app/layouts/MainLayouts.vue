0e0146af851ad9bab8d83e8d558ff3c267817dad
Abayomi Aluko <franicons@gmail.com>
2023-06-14 12:05:35 +0100

    Building sidebar

--- src/app/layouts/MainLayouts.vue
+++ src/app/layouts/MainLayouts.vue
@@ -6,11 +6,17 @@
       <!--sidebar collapse-->
       <NavCollapse/>
       <!--top bar-->
-      <div class="w-full h-full p-8">
-        <!--sidebar toggle-->
-        <div class="flex 2xl:hidden">
-          <i class="pi pi-align-right text-black cursor-pointer hover:text-gray-600" style="font-size: 1.8rem"
-             @click="visible"></i>
+      <div class="w-full h-full">
+        <div class="border-b py-3 px-8 flex justify-end">
+          <!--sidebar toggle-->
+          <div class="flex 2xl:hidden">
+            <i class="pi pi-align-right text-black cursor-pointer hover:text-gray-600" style="font-size: 1.8rem"
+               @click="visible"></i>
+          </div>
+          <!--avatar-->
+          <div class="">
+            <Avatar image="/public/avata.png" shape="circle"/>
+          </div>
         </div>
         <!--main-->
         <section class="h-[calc(100vh-50px)] relative overflow-x-hidden my-5">
@@ -24,11 +30,13 @@
 <script setup lang="ts">
 import navList from "@/app/layouts/menu/menu.ts";
 import otherList from "@/app/layouts/menu/sub-menu";
+
+import Avatar from 'primevue/avatar';
 import SideBar from "@/app/layouts/navbars/SideBar.vue";
 import NavCollapse from "@/app/layouts/navbars/CollapseSideBar.vue";
 
-import { ref } from "vue";
-import { useModalStore } from "@/app/stores/other/modal";
+import {ref} from "vue";
+import {useModalStore} from "@/app/stores/other/modal";
 
 const store = useModalStore()
 const menu = ref(navList);

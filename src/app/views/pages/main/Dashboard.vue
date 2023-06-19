40df4ff9992f60b9ba4e448e33cea3b2d6b2739f
Abayomi Aluko <franicons@gmail.com>
2023-06-19 17:38:33 +0100

    feat: dashboard-#1
    * create-dashboard-page

--- src/app/views/pages/main/Dashboard.vue
+++ src/app/views/pages/main/Dashboard.vue
@@ -2,14 +2,14 @@
   <section class="mb-10 w-full">
     <div class="grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 grid-cols-1 xl:gap-6 lg:gap-8 md:gap-5 gap-0 content-stretch h-full">
       <!--Wallet Balance-->
-      <div class="xl:col-span-1 lg:col-span-2">
+      <div class="xl:col-span-1 lg:col-span-2 h-screen">
         <BalanceCard/>
         <div class="mt-6">
           <OwnedAssets/>
         </div>
       </div>
       <!--Chart & Top Crypto-->
-      <div class="xl:col-span-2 lg:col-span-4 md:col-span-2 lg:order-1 mt-5">
+      <div class="xl:col-span-2 lg:col-span-4 md:col-span-2 lg:order-1 h-screen">
         <div class="p-3 border">
           <div class="grid xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 gap-5">
            <div class=""> <Chart/></div>
@@ -22,13 +22,13 @@
         </div>
       </div>
       <!--loader & end Crypto-->
-      <div class="relative xl:order-1 md:-order-2 xl:col-span-1 lg:col-span-2">
+      <div class="relative xl:order-1 md:-order-2 xl:col-span-1 lg:col-span-2 h-screen">
         <BalanceCard/>
         <div class="mt-6">
           <OwnedAssets/>
         </div>
       </div>
-      <div class="relative xl:order-1 md:-order-2 xl:col-span-1 lg:col-span-2 xl:hidden hidden lg:flex lg:flex-col lg:justify-between">
+      <div class="relative xl:order-1 md:-order-2 xl:col-span-1 lg:col-span-2 xl:hidden hidden lg:flex lg:flex-col lg:justify-between h-screen">
         <div class=""> <TransactionHistory/></div>
         <div class=""><TopCrypto class=""/></div>
       </div>

ef73f3247ff6b0acbe9454c290a49a01bfba56de
Abayomi Aluko <franicons@gmail.com>
2023-06-20 07:37:56 +0100

    feat: dashboard-#1
    * create-dashboard-page

--- src/app/components/Chart.vue
+++ src/app/components/Chart.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="card relative">
+  <div class="card relative my-10">
     <div class="flex justify-between mb-4">
      <div class="order-1">
        <div class="">
@@ -12,11 +12,11 @@
         <div class="font-semibold text-xl">20,000.01</div>
       </div>
     </div>
-    <Chart type="bar" :data="chartData" :options="chartOptions" class="h-80" />
-    <div class="flex justify-between gap-4 text-center mt-4 ">
-      <button class="py-2 bg-gray-950 text-gray-200 w-full font-semibold rounded">Sell</button>
-      <button class="py-2 bg-gray-200 text-gray-900 w-full font-semibold rounded">BUY</button>
-    </div>
+    <Chart type="bar" :data="chartData" :options="chartOptions" class="h-96" />
+<!--    <div class="flex justify-start items-start gap-4 text-center mt-4 ">-->
+<!--      <button class="py-2 bg-gray-950 text-gray-200 w-60  font-semibold rounded">Sell</button>-->
+<!--      <button class="py-2 bg-gray-200 text-gray-900 w-60  font-semibold rounded">BUY</button>-->
+<!--    </div>-->
   </div>
 </template>
 
@@ -36,30 +36,31 @@ const setChartData = () => {
   const documentStyle = getComputedStyle(document.documentElement);
 
   return {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: ['Jan', 'February', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'oct', 'nov', 'dec'],
     datasets: [
       {
         type: 'line',
         label: 'Dataset 1',
-        borderColor: documentStyle.getPropertyValue('surface-900'),
+        borderColor: documentStyle.getPropertyValue('--surface-800'),
         borderWidth: 2,
         fill: false,
         tension: 0.4,
-        data: [50, 25, 12, 48, 56, 76, 42]
+        data: [50, 25, 12, 48, 56, 76, 42, 50, 25, 12, 48, 56, 76, 42]
       },
       {
         type: 'bar',
         label: 'Dataset 2',
         backgroundColor: documentStyle.getPropertyValue('--surface-500'),
-        data: [21, 84, 24, 75, 37, 65, 34],
+        data: [21, 84, 24, 75, 37, 65, 34, 21, 84, 24, 75, 37, 65, 34],
         borderColor: 'white',
         borderWidth: 2
       },
+
       {
         type: 'bar',
         label: 'Dataset 3',
-        backgroundColor: documentStyle.getPropertyValue('--surface-900'),
-        data: [41, 52, 24, 74, 23, 21, 32]
+        backgroundColor: documentStyle.getPropertyValue('--surface-300'),
+        data: [41, 52, 24, 74, 23, 21, 32, 41, 52, 24, 74, 10, 21, 32]
       }
     ]
   };

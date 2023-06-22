7963eb633d4f17772a67e155c9cddfdb1f4782d6
Abayomi Aluko <franicons@gmail.com>
2023-06-22 20:49:22 +0100

    feat: transactions-#14
    * create-transaction-page

--- src/app/components/tables/TransactionTable.vue
+++ src/app/components/tables/TransactionTable.vue
@@ -1,4 +1,18 @@
 <template>
+  <Dialog v-model:visible="visible" modal header="Transaction Details"  class="2xl:w-[45vw] xl:w-[50vw] lg:w-[60vw] md:w-[80vw] w-[90vw]">
+   <div>
+     <p class="pb-2">Transaction Info</p>
+     <div class="border rounded flex ">
+       <ul class="relative overflow-hidden">
+          <li class="flex lg:flex-row flex-col items-center lg:w-[100vw] p-3 lg:p-0  space-y-1 md:space-y-0">
+            <div class="lg:w-[20%] xl:w-[14%] lg:p-4 font-[500] text-gray-500 w-full md:text-[15px] text-[14px]">Transaction Date</div>
+            <div class="border-b-0 md:border-l lg:p-4 border-b w-full font-[500] md:text-[15px] text-[12px]">22 June, 2023 4:34pm</div>
+          </li>
+       </ul>
+     </div>
+   </div>
+  </Dialog>
+
   <div class="card border shadow-sm">
     <DataTable :value="products" removableSort tableStyle="min-width: 50rem">
       <Column field="id" header="Tx ID" sortable style="width: 15%"></Column>
@@ -13,12 +27,16 @@
       <Column field="tx_type" header="Tx Type" sortable style="width: 15%">
         <template #body="slotProps">
           <div>
-            <i class="pi pu-check-circle"></i>
+<!--            <i class="pi pu-check-circle"></i>-->
             <span>{{ slotProps.data.tx_type }}</span>
           </div>
         </template>
       </Column>
-      <Column field="" header="Details"  style="width: 15%"></Column>
+      <Column field="" header="Details"  style="width: 10%">
+        <template #body>
+          <Button icon="pi pi-eye" severity="secondary" text rounded aria-label="Bookmark" @click="visible = true" />
+        </template>
+      </Column>
     </DataTable>
   </div>
 </template>
@@ -26,9 +44,13 @@
 <script setup>
 import Tag from 'primevue/tag';
 import Column from 'primevue/column';
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
 import DataTable from 'primevue/datatable';
+import Divider from 'primevue/divider';
 
-import {transactions} from "@/app/_mock/assets";
+
+import { transactions } from "@/app/_mock/assets";
 
 import { ref } from 'vue';
 
@@ -48,23 +70,9 @@ const getStatusLabel = (status) => {
   }
 };
 
-const getIcon = (status) => {
-  switch (status) {
-    case 'SUCCESSFUL':
-      return 'pi pi-check';
-
-    case 'PENDING':
-      return 'pi pi-exclamation-triangle';
-
-    case 'FAILED':
-      return 'pi pi-times';
-
-    default:
-      return null;
-  }
-};
-
 const products = ref(transactions);
+
+const visible = ref(false);
 
 </script>
 
@@ -79,5 +87,9 @@ tbody {
   font-size: 11px !important;
   font-weight: 600;
   text-align: left !important;
+}
+td {
+  padding: 0 20px !important;
+  margin: 0 !important;
 }
 </style>

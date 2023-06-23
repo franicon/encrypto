ff05da0accb386bd5099e019e5264ffbf7ea2afb
Abayomi Aluko <franicons@gmail.com>
2023-06-23 10:33:45 +0100

    feat: transactions-#14
    * create-transaction-page

--- src/app/stores/main/tx.store.ts
+++ src/app/stores/main/tx.store.ts
@@ -1,14 +1,15 @@
 import {defineStore} from "pinia";
+import type { tx } from "@/types";
 import {transactions} from "@/app/_mock/trancs";
 
 export const useTxStore = defineStore({
     id: 'tx',
     state: () => ({
-        details: null,
+        details: tx ,
         modal: false
     }),
     actions: {
-        get_details(id) {
+        get_details(id: string) {
             transactions.find((tx) => {
               if (tx.id == id) {
                   this.details = tx

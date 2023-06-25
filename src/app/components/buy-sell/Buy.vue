99cb00c5809be2c6c7b5827aff91ab68548ab1cf
Abayomi Aluko <franicons@gmail.com>
2023-06-25 14:13:21 +0100

    feat: buy-sell-23
    * build buy and sell page

--- src/app/components/buy-sell/Buy.vue
+++ src/app/components/buy-sell/Buy.vue
@@ -37,13 +37,14 @@
     </div>
   </div>
 
+  <!--Buy Button-->
   <div class="mt-8 flex justify-center">
-    <button class="w-80 rounded bg-gray-700 hover:bg-gray-800 transition-all duration-500 py-4 text-gray-50 font-[500]"
-            @click="visible = true">Continue
+    <button class="w-56 rounded bg-gray-700 hover:bg-gray-800 transition-all duration-500 py-4 text-gray-50 font-[500]"
+            @click="visible = true">Buy Now
     </button>
   </div>
 
-<!--  Modal Buy-->
+  <!--Modal Buy-->
   <div class="card flex justify-content-center">
     <Dialog v-model:visible="visible" :closable="false" modal header="Confirm Order" class="2xl:w-[30vw] xl:w-[50vw] lg:w-[60vw] md:w-[80vw] w-[90vw]">
       <div>
@@ -53,10 +54,43 @@
           <p class="text-sm">You are about to make a purchase of <br> <span class="text-gray-400 font-[500] pt-2 text-sm">0.0024 BTC @ 1 BTC = 2,873,930</span></p>
         </div>
         <div class="mt-3">
-          <p class="text-md font-[500]">Receiving Address</p>
+          <!--DropDown Wallet-->
+          <div class="mt-8">
+            <p class="text-md font-[500] pb-2">Receiving Address</p>
+            <div class="card flex justify-content-center border px-3 pt-2">
+              <div class="flex flex-col gap-3 w-full relative overflow-hidden">
+                <div v-for="category in wallet" :key="category.key" class="flex align-items-center border-b">
+                  <div class="flex justify-between w-full pb-3">
+                    <div class="flex items-center">
+                        <RadioButton v-model="selectedWallet" :inputId="category.key" :value="category.name"/>
+                       <div class="">
+                         <div class="">
+                           <label :for="category.key" class="ml-2 text-sm">{{ category.name }}</label>
+                           <p class="ml-2 text-xs">{{category.address}}</p>
+                         </div>
+                       </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="absolute bottom-[3px] left-7 w-full"  v-if="selectedWallet === 'Others'">
+                  <InputText  v-model="value" class="p-inputtext-sm w-[93%] address-input" placeholder="bc1qxy2kgdygjrsqtzq2n0yrf2493p83kkfjhx0wlh" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="mt-8">
+            <p class="text-md font-[500] pb-2">Order Details</p>
+            <div class="card flex justify-content-center border px-3 pt-2">
+              <div class="flex flex-col gap-3 w-full relative overflow-hidden">
+
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <ConfirmDialog></ConfirmDialog>
+      <ConfirmDialog>
+      </ConfirmDialog>
       <template #header>
         <div class="flex justify-end w-full"><Button icon="pi pi-times"  text @click="confirm1()"/></div>
       </template>
@@ -68,6 +102,7 @@
 import Button from 'primevue/button';
 import Dialog from 'primevue/dialog';
 import Dropdown from 'primevue/dropdown';
+import InputText from 'primevue/inputtext';
 import InputNumber from 'primevue/inputnumber';
 import RadioButton from 'primevue/radiobutton';
 import ConfirmDialog from 'primevue/confirmdialog';
@@ -82,12 +117,14 @@ const confirm = useConfirm();
 
 const visible = ref(false);
 
-
 const confirm1 = () => {
   confirm.require({
     message: 'Are you sure you want to cancel?',
     header: 'Confirmation',
     icon: 'pi pi-exclamation-triangle',
+    acceptIcon: 'pi pi-check',
+    rejectIcon: 'pi pi-times',
+    acceptButton: 'Yeoooo',
     accept: () => {
       visible.value = false
       toast.add({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted', life: 3000 });
@@ -106,16 +143,23 @@ const cities = ref([
   {name: 'Tether', code: 'USDT', img: 'usdt.svg'},
   {name: 'Shiba Inu', code: 'SHIB', img: 'shiba.webp'}
 ]);
+
 const categories = ref([
-  {name: 'Bank Transfer', key: 'M', icon: 'pi-money-bill'},
-  {name: 'Crypto Transfer', key: 'A', icon: 'pi-sync'},
-  {name: 'Loan', key: 'P', icon: 'pi-image'},
+  {name: 'Paychant', key: 'A', icon: 'pi-sync'},
+  {name: 'Carbon', key: 'M', icon: 'pi-money-bill'},
+  {name: 'PayStack', key: 'P', icon: 'pi-image'},
 
 ]);
+
+const wallet = ref([
+    { name: 'Bitcoin', address: 'bc1qxy2kgdygjrs-------493p83kkfjhx0wlh', img: 'btc.svg'},
+    { name: 'Others', address: '', img: 'btc.svg'},
+])
 
 const amount = ref(2000);
 const selectedCity = ref(cities.value[0]);
 const selectedCategory = ref('Bank Transfer');
+const selectedWallet = ref('Bitcoin')
 
 const logData = () => {
   console.log(selectedCity.value.name, selectedCategory.value, amount.value)
@@ -124,5 +168,8 @@ const logData = () => {
 </script>
 
 <style scoped>
-
+.address-input {
+  border: 0 !important;
+  font-size: small;
+}
 </style>

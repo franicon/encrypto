940d3a994b47a197331333579bbe145dae78ea42
Abayomi Aluko <franicons@gmail.com>
2023-07-04 15:55:30 +0100

    feat: buy-sell-23
    * build buy and sell page

--- src/app/views/pages/main/Orders.vue
+++ src/app/views/pages/main/Orders.vue
@@ -6,7 +6,7 @@
       <div class="pt-6 grid xl:grid-cols-4 grid-cols-1 gap-4">
         <!--NGN-->
         <div class="">
-          <div class="shadow-2xl relative space-y-2">
+          <div class="md:shadow-2xl border relative space-y-2">
             <div class="px-5 pt-5 pb-3 flex items-center space-x-5">
               <i class="pi pi-money-bill text-gray-900" style="font-size: 2rem"></i>
               <span class="font-medium text-md text-gray-600">NGN Account</span>
@@ -16,7 +16,7 @@
               <p class="text-sm font-semibold pt-1 text-gray-600">72.82 USD</p>
             </div>
             <hr>
-            <div class="pb-3 pt-1 px-5">
+            <div class="pb-4 pt-4 px-5">
               <div class=" flex justify-evenly text-center">
                 <div
                     class="w-36 text-xs cursor-pointer border hover:bg-gray-700 hover:text-gray-100 transition-all duration-500 py-3 text-gray-900 font-[400]">
@@ -33,7 +33,7 @@
         </div>
         <!--EUR-->
         <div class="">
-          <div class="shadow-2xl relative space-y-2 bg-gray-800 text-gray-50">
+          <div class="md:shadow-2xl border relative space-y-2 bg-gray-800 text-gray-50">
             <div class="px-5 pt-5 pb-3 flex items-center space-x-5">
               <i class="pi pi-money-bill text-gray-50" style="font-size: 2rem"></i>
               <span class="font-medium text-md text-gray-100">EUR Account</span>
@@ -43,7 +43,7 @@
               <p class="text-sm font-semibold pt-1 text-gray-200">72.82 USD</p>
             </div>
             <hr>
-            <div class="pb-3 pt-1 px-5">
+            <div class="pb-4 pt-4 px-5">
               <div class=" flex justify-evenly text-center">
                 <button class="w-36 text-xs cursor-pointer bg-gray-50 hover:bg-gray-700 hover:text-gray-100 transition-all duration-500 py-3 text-gray-900 font-[400]">
                   Withdraw All Earning
@@ -58,7 +58,7 @@
         </div>
         <!--USD-->
         <div class="">
-          <div class="shadow-2xl relative space-y-2">
+          <div class="md:shadow-2xl border relative space-y-2">
             <div class="px-5 pt-5 pb-3 flex items-center space-x-5">
               <i class="pi pi-money-bill text-gray-900" style="font-size: 2rem"></i>
               <span class="font-medium text-md text-gray-600">USD Account</span>
@@ -68,7 +68,7 @@
               <p class="text-sm font-semibold pt-1 text-gray-600">72.82 USD</p>
             </div>
             <hr>
-            <div class="pb-3 pt-1 px-5">
+            <div class="pb-4 pt-4 px-5">
               <div class=" flex justify-evenly text-center">
                 <div class="w-36 text-xs cursor-pointer border hover:bg-gray-700 hover:text-gray-100 transition-all duration-500 py-3 text-gray-900 font-[400]">
                   Withdraw All Earning
@@ -88,25 +88,25 @@
       <div class="pt-6 grid xl:grid-cols-4 grid-cols-1 gap-4">
         <!--NGN-->
         <div class="">
-          <div class="shadow-2xl relative space-y-2">
+          <div class="md:shadow-2xl border relative space-y-2">
             <div class="px-5 pt-5 pb-3 flex items-center space-x-5">
-              <i class="pi pi-money-bill text-gray-900" style="font-size: 2rem"></i>
-              <span class="font-medium text-md text-gray-600">NGN Account</span>
+<!--              <i class="pi pi-money-bill text-gray-900" style="font-size: 2rem"></i>-->
+              <span class="font-medium text-md text-gray-600">ETH Wallet</span>
             </div>
             <div class="px-5 pb-3">
-              <h1 class="text-2xl font-semibold">56,439.00 NGN</h1>
+              <h1 class="text-2xl font-semibold">0.03524 ETH</h1>
               <p class="text-sm font-semibold pt-1 text-gray-600">72.82 USD</p>
             </div>
             <hr>
-            <div class="pb-3 pt-1 px-5">
+            <div class="pb-4 pt-4 px-5">
               <div class=" flex justify-evenly text-center">
                 <div
                     class="w-36 text-xs cursor-pointer border hover:bg-gray-700 hover:text-gray-100 transition-all duration-500 py-3 text-gray-900 font-[400]">
-                  Withdraw All Earning
+                  Sell Asset
                 </div>
                 <div
                     class="w-36 text-xs cursor-pointer border bg-gray-800 hover:bg-gray-700 hover:text-gray-100 transition-all duration-500 py-3 text-gray-100 font-[400]">
-                  Add Money
+                  Buy Asset
                 </div>
               </div>
             </div>
@@ -115,9 +115,8 @@
         </div>
         <!--USD-->
         <div class="">
-          <div class="shadow-2xl relative space-y-2">
+          <div class="md:shadow-2xl border relative space-y-2">
             <div class="px-5 pt-5 pb-3 flex items-center space-x-5">
-              <i class="pi pi-money-bill text-gray-900" style="font-size: 2rem"></i>
               <span class="font-medium text-md text-gray-600">USD Account</span>
             </div>
             <div class="px-5 pb-3">
@@ -125,13 +124,13 @@
               <p class="text-sm font-semibold pt-1 text-gray-600">72.82 USD</p>
             </div>
             <hr>
-            <div class="pb-3 pt-1 px-5">
+            <div class="pb-4 pt-4 px-5">
               <div class=" flex justify-evenly text-center">
                 <div class="w-36 text-xs cursor-pointer border hover:bg-gray-700 hover:text-gray-100 transition-all duration-500 py-3 text-gray-900 font-[400]">
-                  Withdraw All Earning
+                 Sell Asset
                 </div>
                 <div class="w-36 text-xs cursor-pointer border bg-gray-800 hover:bg-gray-700 hover:text-gray-100 transition-all duration-500 py-3 text-gray-100 font-[400]">
-                  Add Money
+                  Buy Asset
                 </div>
               </div>
             </div>

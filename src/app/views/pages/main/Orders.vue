f04965e46cbfbfd5090a0d3b8156a5a4c1fe863d
Abayomi Aluko <franicons@gmail.com>
2023-07-04 05:25:27 +0100

    feat: buy-sell-23
    * build buy and sell page

--- src/app/views/pages/main/Orders.vue
+++ src/app/views/pages/main/Orders.vue
@@ -1,5 +1,24 @@
 <template>
-  <div class=""></div>
+  <div>
+    <h1 class="text-md">Here are your assets | wallets!</h1>
+    <div class="my-6">
+      <h1 class="text-xl font-semibold">Fiat Wallets</h1>
+      <div class="pt-6 grid grid-cols-4 gap-3">
+        <div class="border shadow-sm relative">
+          <div class="p-5">
+            <i class="pi pi-money-bill text-emerald-500" style="font-size: 2rem"></i>
+          </div>
+          <div class="px-5 pb-3">
+            <h1 class="text-2xl font-semibold">56,439.00 NGN</h1>
+            <p class="text-sm font-semibold pt-1">72.82 USD</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="my-6">
+      <h1 class="text-xl font-semibold">Asset Wallets</h1>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
